2d17d7910b09fd76be77135df4856a0d77dc1dae
Tim Brownawell <tbrownaw@prjek.net>
2019-02-22 11:52:39 -0600

    external library

--- app/app.cc
+++ app/app.cc
@@ -1,6 +1,10 @@
+#include <iostream>
+
 #include "hello.hh"
+#include "external.hh"
 
 int main(int argc, char **argv) {
+    std::cout<<get_number()<<"\n";
     hello();
     return 0;
 }
---
+++ external/external.cc
@@ -0,0 +1,5 @@
+#include "external.hh"
+
+int get_number() {
+    return 42;
+}
